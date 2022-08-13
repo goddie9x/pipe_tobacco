371bded43c508695311195fff901924d15965def
Hoang Tam <goddie9x@yahoo.com>
2022-08-14 00:44:35 +0700

    fix: product

---
+++ App/Models/News.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Models;
+
+class News extends Model
+{
+    protected $table = 'news';
+    protected $primaryKey = 'news_id';
+}
--- views/admin/products/index.php
+++ views/admin/products/index.php
@@ -1,42 +1,76 @@
-<div class="slide-manager">
-    <div class="slide-manager-header">
-        <div class="slide-manager-header-title">
-            <h2>Slides</h2>
+<div class="product-manager">
+    <div class="product-manager-header">
+        <div class="product-manager-header-title">
+            <h2>Products</h2>
         </div>
-        <div class="slide-manager-header-action">
-            <a href="<?= route('create_slide') ?>" class="btn btn-primary">Create Slide</a>
+        <div class="product-manager-header-action">
+            <a href="<?= route('create_product') ?>" class="btn btn-primary">Create Product</a>
         </div>
     </div>
-    <div class="slide-manager-body">
-        <div class="slide-manager-body-table">
+    <div class="product-manager-body">
+        <div class="product-manager-body-table">
             <table class="table table-striped">
                 <thead>
                     <tr>
                         <th>ID</th>
                         <th>Image</th>
                         <th>Name</th>
-                        <th>Path</th>
-                        <th>Status</th>
+                        <th>Price</th>
+                        <th>Is Hot</th>
                         <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <?php foreach ($slides as $slide): ?>
+                    <?php foreach ($products as $product): ?>
                     <tr>
-                        <td><?=$slide['slide_id'] ?></td>
+                        <td><?=$product['product_id'] ?></td>
                         <td>
-                            <img class="w-100" src="<?= url('public/images/slides/') . $slide['slide_image'] ?>" alt="">
+                            <img class="w-100" src="<?= url('public/images/products/') . $product['product_image'] ?>" alt="">
                         </td>
-                        <td><?=$slide['slide_name'] ?></td>
-                        <td><?=$slide['slide_path'] ?></td>
-                        <td><?=$slide['slide_status']==1?'Yes':'No' ?></td>
+                        <td><?=$product['product_name'] ?></td>
+                        <td><?=$product['unit_cost'] ?></td>
+                        <td><?=$product['hot']==1?'Yes':'No' ?></td>
                         <td>
-                            <a href="<?=url('admin/slides/edit/'.$slide['slide_id']) ?>" class="btn btn-primary">Edit</a>
-                            <a href="<?=url('admin/slides/delete/'.$slide['slide_id']) ?>" class="btn btn-danger">Delete</a>
+                            <a href="<?=url('admin/products/edit/'.$product['product_id']) ?>" class="btn btn-primary">Edit</a>
+                            <a href="<?=url('admin/products/delete/'.$product['product_id']) ?>" class="btn btn-danger">Delete</a>
                         </td>
                     </tr>
                     <?php endforeach; ?>
                 </tbody>
+                <tfoot>
+                    <tr>
+                        <td colspan="5">
+                            <ul class="pagination">
+                                <?php if($pageNumber - 1 > 0):?>
+                                <li class="page-item <?= $pageNumber == 1 ? 'disabled' : '' ?>">
+                                    <a class="page-link"
+                                        href="<?= url('admin/products?pageNumber=') . ($pageNumber) ?>">Previous</a>
+                                </li>
+                                <li class="page-item <?= $pageNumber == 1 ? 'disabled' : '' ?>">
+                                    <a class="page-link"
+                                        href="<?= url('admin/products?pageNumber=') . ($pageNumber) ?>">1</a>
+                                </li>
+                                <?php endif; ?>
+                                <?php if($pageNumber+1>0): ?>
+                                <li class="page-item <?= $pageNumber == 1 ? 'disabled' : '' ?>">
+                                    <a class="page-link"
+                                        href="<?= url('admin/products?pageNumber=') . ($pageNumber) ?>"><?= $pageNumber ?></a>
+                                </li>
+                                <?php endif; ?>
+                                <?php if($totalPage>1): ?>
+                                <li class="page-item <?= $pageNumber == $totalPage ? 'disabled' : '' ?>">
+                                    <a class="page-link"
+                                        href="<?= url('admin/products?pageNumber=') . ($pageNumber + 1) ?>">Next</a>
+                                </li>
+                                <li class="page-item <?= $pageNumber == $totalPage ? 'disabled' : '' ?>">
+                                    <a class="page-link"
+                                        href="<?= url('admin/products?pageNumber=') . $totalPage ?>"><?= $totalPage ?></a>
+                                </li>
+                                </li>
+                                <?php endif; ?>
+                            </ul>
+                        </td>
+                </tfoot>
             </table>
         </div>
     </div>
